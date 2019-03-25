12998e0b14f42c86af5a89ce716ed9fabe0f9a35
Patrick Sheriddan <sheridp2@gmail.com>
2019-03-25 13:13:15 -0700

    session and user factories added to test file, generates new user each time tests are run, setup file in test gives acces to mongoose User model and config keys

--- test/header.test.js
+++ test/header.test.js
@@ -1,4 +1,6 @@
 const puppeteer = require("puppeteer");
+const sessionFactory = require('./factories/sessionFactory')
+const userFactory = require('./factories/userFactory')
 
 let browser, page;
 
@@ -29,23 +31,10 @@ test("clicking login starts oauth flow", async () => {
 });
 
 test("When signed in show logout button", async () => {
-  const id= '5c92a45e6d8a8f34bbf0006a';
+  const user = await userFactory()
+  const { session, sig } = sessionFactory(user);
 
-  const Buffer = require('safe-buffer').Buffer;
-  const sessionObject = {
-    passport: {
-      user: id
-    }
-  }
-
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64')
-
-  const Keygrip = require('keygrip');
-  const keys = require('../config/keys')
-  const keygrip = new Keygrip([keys.cookieKey])
-  const sig = keygrip.sign('session=' + sessionString)
-
-  await page.setCookie({ name: 'session', value: sessionString })
+  await page.setCookie({ name: 'session', value: session })
   await page.setCookie({ name: 'session.sig', value: sig})
   await page.goto('localhost:3000');
   await page.waitFor('a[href="/auth/logout"]')
