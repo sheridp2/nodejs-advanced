bfc06d596a9574292de29bf52c85375f8673d89f
Patrick Sheriddan <sheridp2@gmail.com>
2019-04-02 17:34:26 -0700

    refactor of logic into page helper, form tests written

---
+++ test/blogs.test.js
@@ -0,0 +1,35 @@
+const Page = require('./helpers/page')
+
+let page
+
+beforeEach(async ()=>{
+  page = await Page.build()
+  await page.goto('localhost:3000')
+})
+
+afterEach(async () =>{
+  await page.close();
+})
+
+describe('When logged in', async () =>{
+  beforeEach(async () =>{
+    await page.login()
+    await page.click('a.btn-floating')
+  })
+
+  test('can see blog create form', async () =>{
+
+    const label = await page.getContentsOf('form label')
+
+    expect(label).toEqual('Blog Title')
+  })
+
+  describe('And using invalid inputs', async () =>{
+    beforeEach(async () =>{
+      await page.click('form button')
+    })
+    test('the form shows an error message', async () =>{
+      
+    })
+  })
+})
